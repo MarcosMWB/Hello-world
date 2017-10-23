2a1253b2a37d327c2736f36d690afbf63e9ddd26
MarcosMWB <marcosmwb1919@gmail.com>
2017-10-23 21:41:53 -0200

    Create a new heap
    Not finished

---
+++ NewHeap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+Typedef struct _heap{
+ Int max;
+ Int pos;
+ Float* prioridade;
+ } Heap;
+ 
+Heap* newheap(int max){
+ Heap* heap=(Heap*)malloc(sizeof(Heap));
+ Heap->max=max;
+ Heap->pos=0;
+ Heap->prioridade=(float*
+}
