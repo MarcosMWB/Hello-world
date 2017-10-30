4e6fbe6d0c2e16ddf6fa501ea8523171065f7ea1
MarcosMWB <marcosmwb1919@gmail.com>
2017-10-30 21:46:27 -0200

    Update and rename Insereheap.c to Removeheap.c

---
+++ Removeheap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+Typedef struct aheap{
+ Int max;
+ Int pos;
+ Float* prioridade;
+ } Heap;
+
+Static void corrige_abaixo(Heap* heap_hurra){
+  Int pai=0;
+  
+
+Float heap_remove(Heap* heap_hurra){
+  If(heap->
+     
+ A funcao imprime na tela as 3 maiores prioridade obs estao disponiveis as funcoes cria, insire, retira, vazio, cheio.
+ B considerando q n ha nenhuma func disponivel, crie uma funcao q retorne a menor prioridade obs aproveitar ordenacao da lista
+ 
